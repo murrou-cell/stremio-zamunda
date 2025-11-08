c80ce44748feb81c0d73eb7f68d7bcfb1a8246ba
Marin Dragolov <62943307+murrou-cell@users.noreply.github.com>
2025-11-08 14:22:20 +0200

    fix: search series episodes by season/episode code (SxxExx) instead o… (#2)
    * fix: search series episodes by season/episode code (SxxExx) instead of title
    
    * fix requests version

--- omdb.py
+++ omdb.py
@@ -3,23 +3,14 @@ import requests
 class Omdb:
     def __init__(self,logger):
         self.logger = logger
-
-    def get_series_title(self,imdbId,season,episode,OMDBkey):
-        url = f"https://www.omdbapi.com/?i={imdbId}&season={season}&episode={episode}&apikey={OMDBkey}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return data["Title"]
-        else:
-            self.logger.info(f"Error: Could not connect to OMDB API - {response.status_code}")
-            return None
         
-    def get_movie_title(self,imdbId,OMDBkey):
+    def get_title(self,imdbId,OMDBkey):
         url = f"https://www.omdbapi.com/?i={imdbId}&apikey={OMDBkey}"
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         if response.status_code == 200:
             data = response.json()
             return data["Title"]
         else:
             self.logger.info(f"Error: Could not connect to OMDB API - {response.status_code}")
             return None
+    
